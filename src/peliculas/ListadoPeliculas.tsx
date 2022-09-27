ba2f3e3cf0442cca6d7cd4e28226a30f1bcefbfc
ANDRES GONZALEZ <andres.aerogp@hotmail.com>
2022-09-27 19:02:05 +0200

    Primeros componentes creados

---
+++ src/peliculas/ListadoPeliculas.tsx
@@ -0,0 +1,23 @@
+import PeliculaIndividual from "./PeliculaIndividual";
+import { pelicula } from "./peliculas.model";
+import css from "./ListadoPeliculas.module.css"
+import Cargando from "../utils/Cargando";
+import ListadoGenerico from "../utils/ListadoGenerico";
+
+export default function ListadoPeliculas(props: listadoPeliculasProp) {
+
+    return (
+        <ListadoGenerico listado={props.peliculas}>
+            <div className={css.div}>
+                {props.peliculas?.map( pelicula => 
+                    <PeliculaIndividual pelicula={pelicula}
+                                        key={pelicula.id}
+                    />)}
+            </div>
+        </ListadoGenerico>        
+    )
+}
+
+interface listadoPeliculasProp{
+    peliculas?: pelicula[];
+}
---
+++ src/utils/Cargando.tsx
@@ -0,0 +1,3 @@
+export default function Cargando() {
+    return <img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif?20151024034921" alt="loading gif"></img>
+}
