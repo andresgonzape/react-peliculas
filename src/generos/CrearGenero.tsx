69b96b01592fb38232ba7237bf2c9d12ab893a84
ANDRES GONZALEZ <andres.aerogp@hotmail.com>
2022-10-01 17:24:48 +0200

    Primer formulario con la libreria formik

--- src/generos/CrearGenero.tsx
+++ src/generos/CrearGenero.tsx
@@ -1,15 +1,37 @@
-import { useNavigate } from "react-router-dom";
+import { Field, Form, Formik } from "formik";
+import { Link, useNavigate } from "react-router-dom";
 import Button from "../utils/Button";
 
 export default function CrearGenero(){
 
     //en el curso, se utiliza useHistory que es un poco diferente, pero muy similar, por ser la v5 en vez de la v6 el react router dom
-    const history = useNavigate();
+    //const history = useNavigate();
 
     return (
         <>
             <h3>Crear Género</h3>
-            <Button onClick={() => history('/generos')}>Guardar</Button>
+
+            <Formik initialValues={{
+                nombre: 'Acción'
+
+            }}
+            onSubmit={ values => {
+                console.log(values);
+            }}
+            >
+                <Form>
+                    <div className = "form-group">
+                        <label htmlFor="nombre">Nombre</label>
+                        <Field name="nombre" className="form-control"></Field> 
+                    </div>
+                    <Button type="submit">Guardar</Button>
+                    <Link className="btn btn-secondary" to="/generos">Cancelar</Link>
+                </Form>
+            </Formik>
+            
+            {/* <Button onClick={() => history('/generos')}>Guardar</Button> */}
+
+            
         </>
         
     )
--- src/utils/Button.tsx
+++ src/utils/Button.tsx
@@ -2,11 +2,16 @@ import { ReactNode } from "react";
 
 export default function Button(props: buttonProps){
     return (
-        <button type='button' className='btn btn-primary' onClick={props.onClick}>{props.children}</button>
+        <button type={props.type} className='btn btn-primary' onClick={props.onClick}>{props.children}</button>
     )
 }
 
 interface buttonProps {
     children: ReactNode,
-    onClick(): void;
+    onClick?(): void,
+    type: "button" | "submit"
+}
+
+Button.defaultProps = {
+    type: "button"
 }
