afddb152030cc42aa6b26fca25034a924e051fa1
ANDRES GONZALEZ <andres.aerogp@hotmail.com>
2022-10-09 11:47:10 +0200

    filtro peliculas w/ select, chkbox y getfieldprops

--- src/generos/generos.model.d.ts
+++ src/generos/generos.model.d.ts
@@ -1,3 +1,9 @@
 export interface generoCreacionDTO {
     nombre: string;
 }
+
+//Este es para lectura, el de arriba para creacion. Este de aqui deberia ser cogido de base de datos en principio
+export interface generoDTO {
+    id: number;
+    nombre: string;
+}
