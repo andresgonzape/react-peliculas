dbaf9bfffe1e32e2d2da71d23a4de0abec95a2b2
ANDRES GONZALEZ <andres.aerogp@hotmail.com>
2023-07-08 19:49:05 +0200

    Conectado con axios al backend

--- src/generos/IndiceGeneros.tsx
+++ src/generos/IndiceGeneros.tsx
@@ -1,6 +1,16 @@
+import { useEffect } from "react";
+import axios, { AxiosResponse } from "axios";
 import { Link } from "react-router-dom";
+import { generoDTO } from "./generos.model";
+import { urlGeneros } from "../utils/endpoints";
 
 export default function IndiceGeneros() {
+    useEffect(()=>{
+        axios.get(urlGeneros)
+        .then((respuesta: AxiosResponse<generoDTO[]>) => {
+            console.log(respuesta.data);
+        })
+    }, [])
     return (
         <>
             <h3>Índice Géneros</h3>
